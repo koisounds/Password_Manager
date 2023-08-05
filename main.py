7401c3251785a83f496ba248b72175cb85e4898e
koisounds <n.alexgalotti@gmail.com>
2023-08-05 18:33:36 -0500

    reorganizing files

--- main.py
+++ main.py
@@ -16,13 +16,11 @@ logo = PhotoImage(file="logo.png")
 canvas.create_image(100,100, image=logo)
 canvas.grid(column=1, row=0)
 
-
+#Labels
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
